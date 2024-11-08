18d96f3044a6c0d57f259b4f74ab6da8e7a68219
Milad Coder <miladcoder1390@gmail.com>
2024-11-08 14:09:48 +0330

    Configure database

--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
 require("dotenv").config();
 
 const express = require('express');
-
 const cors = require('cors');
-
 const app = express();
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
@@ -14,5 +13,15 @@ cors({
     method: ["GET", "POST", "DELETE", 'PUT'],
     allowedHeaders: ['Content-Type', 'Authorization']
 });
+
+
+
+mongoose.connect(MONGO_URI)
+    .then(
+        () =>{console.log('mongodb is connected')}
+    )
+    .catch(
+        (e) => {console.log(e)}
+    )
 
 app.use(express.json())
