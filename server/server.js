dbfcdd03f1a005a91a3ed6fd839d9b0aed6557ec
Milad Coder <miladcoder1390@gmail.com>
2024-11-08 14:17:55 +0330

    Add comment & Increasing code readability

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 require("dotenv").config();
 
+// ! Add Packages
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
 
+// ! Receive Port & Mongodb URI form enviroment  
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
+
+// ! Set security configs
 cors({
     origin: process.env.CLIENT_URI,
     method: ["GET", "POST", "DELETE", 'PUT'],
@@ -15,13 +19,13 @@ cors({
 });
 
 
-
+// ! Connect to database(mongodb)  
 mongoose.connect(MONGO_URI)
     .then(
-        () =>{console.log('mongodb is connected')}
+        () => { console.log('mongodb is connected') }
     )
     .catch(
-        (e) => {console.log(e)}
+        (e) => { console.log(e) }
     )
 
 app.use(express.json())
