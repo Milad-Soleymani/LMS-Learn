4d942bdb724d0f9711cfe9fbbdc1c23a73e8c184
Milad Coder <miladcoder1390@gmail.com>
2024-11-11 13:44:21 +0330

    Configure & Debug  Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,12 +6,11 @@ import { Button } from './components/ui/button'
 import { Route, Routes } from 'react-router-dom'
 import AuthPage from './pages/auth'
 
-
 function App() {
   return (
-  <Routes>
-    <Route path='/auth' Component={<AuthPage />}/>
-  </Routes>
+    <Routes>
+      <Route path='/auth' element={<AuthPage />} />
+    </Routes>
   )
 }
 
