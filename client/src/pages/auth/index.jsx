defe66d77860cf56aafb424452eab389e58914e0
Milad Coder <miladcoder1390@gmail.com>
2024-11-11 14:21:43 +0330

    Add Web Header

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -1,9 +1,16 @@
+import { GraduationCap } from 'lucide-react'
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 const AuthPage = () => {
     return (
-        <div>
-            <p>auth</p>
+        <div className='flex flex-col min-h-screen'>
+            <header className='flex items-center px-4 border-b lg:px-6 h-14 '>
+                <Link to={'/'} className='flex items-center justify-center' />
+                <GraduationCap className='w-8 h-8 mr-4'/>
+                <span className='text-xl font-extrabold'> LMS LEARN </span>
+            </header>
         </div>
     )
 }
