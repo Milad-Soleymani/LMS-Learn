fac2587c776a03cabd21d2efc8f1f5252877a7ee
Milad Coder <miladcoder1390@gmail.com>
2024-11-11 15:24:05 +0330

    Edit UI

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom'
 const AuthPage = () => {
 
     const [activeTab, setActiveTab] = React.useState('signin')
-    function handleTapChange (value) {
+    function handleTapChange(value) {
         setActiveTab(value)
     }
 
@@ -15,15 +15,15 @@ const AuthPage = () => {
         <div className='flex flex-col min-h-screen'>
             <header className='flex items-center px-4 border-b lg:px-6 h-14 '>
                 <Link to={'/'} className='flex items-center justify-center' />
-                <GraduationCap className='w-8 h-8 mr-4'/>
+                <GraduationCap className='w-8 h-8 mr-4' />
                 <span className='text-xl font-extrabold'> LMS LEARN </span>
             </header>
             <div className='flex items-center justify-center min-h-screen bg-background'>
                 <Tabs
-                value={activeTab}
-                defaultChecked='singin'
-                onValueChange={handleTapChange}
-                className='w-full max-w-md' >
+                    value={activeTab}
+                    defaultChecked='singin'
+                    onValueChange={handleTapChange}
+                    className='w-full max-w-md' >
                     <TabsList className='grid w-full grid-cols-2'>
                         <TabsTrigger value='signin'> Sign In </TabsTrigger>
                         <TabsTrigger value='signup'> Sign Up </TabsTrigger>
