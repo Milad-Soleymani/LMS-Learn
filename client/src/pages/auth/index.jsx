7d13cb3b0d2f2febb6e1ca99ca3cacf2cc4ad2ef
Milad Coder <miladcoder1390@gmail.com>
2024-11-11 13:34:30 +0330

    Add first Route

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const AuthPage = () => {
-  return (
-    <div>
-      auth
-    </div>
-  )
+    return (
+        <div>
+            <p>auth</p>
+        </div>
+    )
 }
 
 export default AuthPage
